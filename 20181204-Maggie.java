382c9412de8517d00aaee55dbdb91ec46e6f4401
HeQiuting <45375773+HeQiuting@users.noreply.github.com>
2018-12-04 09:43:16 +0800

    Create 20181204-Maggie.java

---
+++ 20181204-Maggie.java
@@ -0,0 +1,24 @@
+//Past Paper 2015
+//main idea: String, class
+public class HiddenWord{
+  private puzzle;
+  public HiddenWord(String s){
+    puzzle = s;
+  }
+  public String getHint(String guess){
+  String tmp = "";
+    for(int i = 0; i < guess.length(); i++){
+      if(guess.substring(i, i+1).equals(puzzle.substring(i, i+1))
+      {
+        tmp += guess.substring(i, i+1);
+      }
+      else if(puzzle.indexOf(s.substring(i, i+1)) == -1)
+      {
+        tmp += "*";
+      }
+      else
+      {
+        tmp += "+";
+      }
+    }
+    return tmp;
